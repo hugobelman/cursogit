485cf04d0237463dd2ac0c50713ad035c9fc9cad
Hugo Belman <hugobelman@outlook.com>
2020-09-23 13:25:23 -0500

    Bug arreglado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 a = input('Ingresa el lado del cuadrado: ')
 
-if (type(a) == type(str)):
-    print('Usted no puede hacer semejante barbaridad')
-else:
-    a = int(a)
+try:
+    a = float(a)
     print(f'Area del cuadrado es {a*a}')
+except:
+    print('Usted no puede hacer semejante barbaridad')
